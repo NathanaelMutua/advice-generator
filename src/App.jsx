85f2e8e90de69e1587a278d6bb228e83e56d4603
nxthxnael <nathanael.mutua.m@gmail.com>
2025-06-23 19:08:51 +0300

    feature: add data fetching from advice slip API - add and import useEffect() to asynchonously fetch data from the API - add variables to hold the data - update the state variable 'advice' to the fetched advice - add a catch error handling to display incase of an error - have an empty dependency array to ensure the advice is rendered with the page, without any initialization

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [advice, setAdvice] = useState(null);
+
+  useEffect(() => {
+    try {
+      async function fetchAdvice() {
+        const response = await fetch(`https://api.adviceslip.com/advice`);
+        const data = await response.json();
+        const advice = data.slip.advice;
+        setAdvice(advice);
+      }
+
+      fetchAdvice();
+    } catch (e) {
+      console.log(`There was an error: ${e}`);
+    }
+  }, []);
+
   return (
     <div className="advice-generator-body">
       <h1 className="title">Advice Generator</h1>
-      <p className="gen-advice">Don't put all your eggs in one basket</p>
+      <p className="gen-advice">{advice}</p>
       <button className="advice-btn">Free Advice</button>
     </div>
   );
