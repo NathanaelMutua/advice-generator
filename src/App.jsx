cbddec93a604aebb1cda237de01eb7f0caa6dc76
nxthxnael <nathanael.mutua.m@gmail.com>
2025-06-23 18:30:50 +0300

    style: add styling to App.jsx components - add styling in App.css to the placeholders in App.jsx - add a className to the title heading

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1>Advice Generator</h1>
+    <div className="advice-generator-body">
+      <h1 className="title">Advice Generator</h1>
       <p className="gen-advice">Don't put all your eggs in one basket</p>
       <button className="advice-btn">Free Advice</button>
-    </>
+    </div>
   );
 }
 
