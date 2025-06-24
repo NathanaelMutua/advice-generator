7bb63e774b1f810dfecfd273e72a7e255d2ead2c
nxthxnael <nathanael.mutua.m@gmail.com>
2025-06-24 22:27:20 +0300

    feat: add multiple fetch for advice - add temporary function inside the fetch advice to carry out loop for fetch - add map through adviceList to display it as a list in the main component

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ import Loader from "./components/Loader";
 
 function App() {
   const [advice, setAdvice] = useState(null);
+  const [adviceList, setAdviceList] = useState([]);
   const [errorMessage, setErrorMessage] = useState(null);
   const [loading, setLoading] = useState(false);
+
+  console.log;
 
   // the use effect will call on the handleFetchData function on each render
   useEffect(() => {
@@ -18,6 +21,23 @@ function App() {
     async function fetchAdvice() {
       try {
         const response = await fetch(`https://api.adviceslip.com/advice`);
+
+        // I want to loop through and try and display multiple advices
+        const fetchSeveralAdvice = async () => {
+          const advicesList = []; // initiate an empty array for the advices
+
+          // the use of await instead of Promise.all is unconventionally slow but we will upgrade
+          for (let i = 0; i < 4; i++) {
+            const responses = await fetch(
+              `https://api.adviceslip.com/advice?timestamp=${Date.now() + i}` // the ?timestamp=${Date.now() + i} ensures/ tricks the API to think it's a new url each time
+            );
+            const datum = await responses.json();
+            advicesList.push(datum.slip.advice); // push the json data to the array
+          }
+          setAdviceList(advicesList);
+        };
+
+        fetchSeveralAdvice();
 
         if (!response.ok) {
           setErrorMessage("Something Went Wrong When Fetching Data!");
@@ -47,8 +67,17 @@ function App() {
           <img src="/message.png" alt="error-message" className="error-img" />
         </>
       )}
-      {advice && <p className="gen-advice">{advice}</p>}
       {/* Advice is only shown if it exists */}
+      {advice && <p className="gen-advice">{advice}</p>}
+      {adviceList ? (
+        <ul>
+          {adviceList.map((advice2, idx) => (
+            <li key={idx}>{advice2}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>Error in Multiple Fetch</p>
+      )}
       <button
         className="advice-btn"
         onClick={handleFetchData}
